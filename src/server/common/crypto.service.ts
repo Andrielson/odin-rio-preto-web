7c9c0a5e8d3572806ae0600770122e1cb580dbe0
Andrielson Silva <andrielson@gmail.com>
2021-10-05 18:35:10 -0300

    Refatoração do serviço de criptografia para maior performance

--- src/server/common/crypto.service.ts
+++ src/server/common/crypto.service.ts
@@ -1,67 +1,63 @@
 import crypto from "crypto";
 import { CryptoService } from "./crypto-service.interface";
 
+if (!process.env.CRYPTO_CIPHER_KEY)
+  throw new Error(
+    "Please define the CRYPTO_CIPHER_KEY environment variable inside .env.local"
+  );
+
+if (!process.env.CRYPTO_CIPHER_KEY_ENCODING)
+  throw new Error(
+    "Please define the CRYPTO_CIPHER_KEY_ENCODING environment variable inside .env.local"
+  );
+
+const CIPHER_KEY_ENCODING = process.env
+  .CRYPTO_CIPHER_KEY_ENCODING as BufferEncoding;
+const CIPHER_KEY = Buffer.from(
+  String(process.env.CRYPTO_CIPHER_KEY),
+  CIPHER_KEY_ENCODING
+);
+
 const CIPHER_ALGORITHM = process.env.CRYPTO_CIPHER_ALGORITHM ?? "aes-128-cbc";
 const CIPHER_DELIMITER = process.env.CRYPTO_CIPHER_DELIMITER ?? "|";
 const CIPHER_ENCODING = (process.env.CRYPTO_CIPHER_ENCODING ??
   "base64") as BufferEncoding;
-const CIPHER_KEY_LENGTH = Number(process.env.CRYPTO_CIPHER_KEY_LENGTH ?? "16");
 const HASH_ALGORITHM = process.env.CRYPTO_HASH_ALGORITHM ?? "sha256";
 
-if (!process.env.CRYPTO_CIPHER_PASSWORD_HEX)
-  throw new Error(
-    "Please define the CRYPTO_CIPHER_PASSWORD_KEY environment variable inside .env.local"
-  );
-const password = Buffer.from(
-  String(process.env.CRYPTO_CIPHER_PASSWORD_HEX),
-  "hex"
-);
-
-const generateKey = (salt: Buffer) =>
-  new Promise<Buffer>((resolve, reject) =>
-    crypto.scrypt(password, salt, CIPHER_KEY_LENGTH, (err, derivedKey) => {
-      if (!!err) reject(err);
-      resolve(derivedKey);
-    })
-  );
-
-const createCipheriv = async () => {
-  const salt = crypto.randomBytes(24);
+const createCipheriv = () => {
   const iv = crypto.randomBytes(16);
-  const key = await generateKey(salt);
-  const cipher = crypto.createCipheriv(CIPHER_ALGORITHM, key, iv);
-  return { cipher, iv, salt };
+  const cipher = crypto.createCipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
+  return { cipher, iv };
 };
 
-const createDecipheriv = async (iv: Buffer, salt: Buffer) => {
-  const key = await generateKey(salt);
-  return crypto.createDecipheriv(CIPHER_ALGORITHM, key, iv);
+const createDecipheriv = (iv: Buffer) => {
+  return crypto.createDecipheriv(CIPHER_ALGORITHM, CIPHER_KEY, iv);
 };
 
 export function CryptoServiceImpl(): CryptoService {
   const encrypt = async (message: string) => {
-    const { cipher, iv, salt } = await createCipheriv();
+    const { cipher, iv } = createCipheriv();
     let value = cipher.update(message, "utf-8");
     value = Buffer.concat([value, cipher.final()]);
-    return [iv, salt, value]
+    return [iv, value]
       .map((it) => it.toString(CIPHER_ENCODING))
       .join(CIPHER_DELIMITER);
   };
 
   const decrypt = async (text: string) => {
-    const [iv, salt, value] = text
+    const [iv, value] = text
       .split(CIPHER_DELIMITER)
       .map((it) => Buffer.from(it, CIPHER_ENCODING));
-    const decipher = await createDecipheriv(iv, salt);
+    const decipher = createDecipheriv(iv);
     let decryptedBuffer = decipher.update(value);
     decryptedBuffer = Buffer.concat([decryptedBuffer, decipher.final()]);
     return decryptedBuffer.toString("utf-8");
   };
 
-  const digest = (text: string) => {
+  const digest = async (text: string) => {
     const hash = crypto.createHash(HASH_ALGORITHM);
     hash.update(text);
-    return Promise.resolve(hash.digest("hex"));
+    return hash.digest("hex");
   };
 
   return { encrypt, decrypt, digest };
