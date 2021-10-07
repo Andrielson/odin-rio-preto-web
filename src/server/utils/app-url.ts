e921741cae0d882c55ac39e75bbfb16635973384
Andrielson Silva <andrielson@gmail.com>
2021-10-07 01:58:51 -0300

    Inclusão do prefixo https na variável de ambiente VERCER_URL

--- src/server/utils/app-url.ts
+++ src/server/utils/app-url.ts
@@ -1,6 +1,11 @@
-if (!process.env.APP_URL && !process.env.VERCEL_URL)
+let APP_URL: string;
+
+if (!!process.env.APP_URL) APP_URL = process.env.APP_URL;
+else if (!!process.env.VERCEL_URL)
+  APP_URL = `https://${process.env.VERCEL_URL}`;
+else
   throw new Error(
     "Please define the APP_URL environment variable inside .env.local"
   );
-const APP_URL = (process.env.APP_URL ?? process.env.VERCEL_URL) as string;
+
 export default APP_URL;
