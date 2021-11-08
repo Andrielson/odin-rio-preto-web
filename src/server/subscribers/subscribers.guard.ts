5a286b46659924ef763fec98de7c1c891104c49c
Andrielson Silva <andrielson@gmail.com>
2021-11-08 17:46:31 -0300

    Correção da guarda

--- src/server/subscribers/subscribers.guard.ts
+++ src/server/subscribers/subscribers.guard.ts
@@ -2,15 +2,28 @@ import { NextApiRequest } from "next";
 import * as jwt from "jsonwebtoken";
 import { Guard } from "@server/types/guard";
 
+const missing = ["OAUTH_AUDIENCE", "OAUTH_ISSUER", "OAUTH_SECRET_KEY"]
+  .filter((it) => !process.env[it])
+  .join(", ");
+if (!!missing) {
+  throw new Error(
+    `Please define the ${missing} environment variable(s) inside .env.local`
+  );
+}
+
+const secretKey = process.env.OAUTH_SECRET_KEY!;
+const audience = process.env.OAUTH_AUDIENCE;
+const issuer = process.env.OAUTH_ISSUER;
+
+const verifyToken = (token: string) =>
+  jwt.verify(token, secretKey, { audience, issuer });
+
 export function SubscribersGuard(): Guard {
   const canActivate = (req: NextApiRequest) => {
     const authorizationHeader = req.headers.authorization ?? "Bearer token";
     const token = authorizationHeader.split(" ")[1];
     try {
-      jwt.verify(token, "25nOkcpvLorpsSBrV1LmTbAslWbojrkZ", {
-        audience: "https://riopreto.diario.tk/api",
-        issuer: "https://dev-1tg5qqmy.us.auth0.com/",
-      });
+      verifyToken(token);
     } catch (error) {
       return Promise.reject(false);
     }
