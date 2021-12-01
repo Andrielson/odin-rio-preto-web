2ed4705b4d430872419bb1e8fb99ee9120644be9
Andrielson Silva <andrielson@gmail.com>
2021-11-30 23:04:13 -0300

    Adição do link da política no rodapé

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,16 @@
 import type { ReactElement } from "react";
+import Link from "next/link";
 import styles from "../styles/Header.module.css";
 
 export default function Header(): ReactElement {
   return (
     <header className={styles.header}>
       <div className={styles.title_box}>
-        <h1 className={styles.title_1}>Boletim do Diário Oficial</h1>
+        <h1 className={styles.title_1}>
+          <Link href="/">
+            <a>Boletim do Diário Oficial</a>
+          </Link>
+        </h1>
         <h2 className={styles.title_2}>São José do Rio Preto-SP</h2>
       </div>
     </header>
